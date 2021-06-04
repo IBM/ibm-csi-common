9451406c93ad4cf6e4d4f3b04cb37b81a390768a
Prankul Mahajan <prankul.mahajan@ibm.com>
2021-06-04 14:32:27 +0530

    Remove proto file + change csi-common ref

--- scripts/handleFailure.sh
+++ scripts/handleFailure.sh
@@ -16,11 +16,11 @@
 # *****************************************************************************/
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_GO_VERSION" == "tip" ]; then
-	curl -s -k -X GET -H "Content-Type: application/json" -H "Accept: application/vnd.travis-ci.2+json"  -H "Authorization: token $TRAVIS_TOKEN"  https://travis-ci.com/IBM/ibmcloud-volume-vpc/builds/$TRAVIS_BUILD_ID | jq '.jobs[0].state' | sed 's/"//g'> state.out
+	curl -s -k -X GET -H "Content-Type: application/json" -H "Accept: application/vnd.travis-ci.2+json"  -H "Authorization: token $TRAVIS_TOKEN"  https://travis-ci.com/IBM/ibm-csi-common/builds/$TRAVIS_BUILD_ID | jq '.jobs[0].state' | sed 's/"//g'> state.out
 	RESULT=$(<state.out)
 	if [ "$RESULT" != "failed" ]; then
 		RESULT_MESSAGE=":warning: Build failed with **tip** version."
-		curl -X POST -H "Authorization: token $GHE_TOKEN" https://github.com/IBM/ibmcloud-volume-vpc/repos/$TRAVIS_REPO_SLUG/issues/$TRAVIS_PULL_REQUEST/comments -H 'Content-Type: application/json' --data '{"body": "'"$RESULT_MESSAGE"'"}'
+		curl -X POST -H "Authorization: token $GHE_TOKEN" https://github.com/IBM/ibm-csi-common/repos/$TRAVIS_REPO_SLUG/issues/$TRAVIS_PULL_REQUEST/comments -H 'Content-Type: application/json' --data '{"body": "'"$RESULT_MESSAGE"'"}'
 	fi
 fi
 exit 0
