f83df31ce1e1c382bd4901857d11b9f739d3119c
GunaKKIBM <92353386+GunaKKIBM@users.noreply.github.com>
2022-09-13 16:16:44 +0530

    Snapshot e2e - considering default addon version (#53)
    * Snapshot e2e - considering default addon version
    
    * Addressing comments
    
    * More logging

--- tests/snapshote2e/snapshot-e2e.sh
+++ tests/snapshote2e/snapshot-e2e.sh
@@ -34,64 +34,6 @@ error() {
      fi
 }
 
-# fetchExpectedVersion() fetches the addon version to be enabled
-fetchExpectedVersion() {
-        # Fetches all the addon versions after 4.+ into an array addonVersions
-        IFS=$'\n' read -r -d '' -a addonVersions < <( ibmcloud ks cluster addon versions | grep vpc-block-csi-driver  | awk '{print $2}' | grep -v "4." )
-
-        # Listing the addon versions for debugging purpose
-        echo "Existing addon versions"
-        echo ${addonVersions[@]}
-
-        # If there is only one addon version, we use that
-        if [[ "${#addonVersions[@]}" -eq 1 ]]; then
-                EXPECTED_VERSION=${addonVersions[0]}
-                return 0
-        fi
-
-        # Fetching the latest addon version
-        latest=0
-        for addonVersion in "${addonVersions[@]}";
-        do
-                # Ignoring if it is beta
-                if [[ "$addonVersion" == *"beta"* ]]; then
-                        continue
-                fi
-
-                # Parsing the version after 5.+
-                IFS=$'.' read -r -d '' -a version < <( echo "$addonVersion" )
-                if [[ ${version[1]} -ge latest ]]; then
-                        latest=${version[1]}
-                        EXPECTED_VERSION=$addonVersion
-                fi
-        done
-
-        if [[ "$EXPECTED_VERSION" == "" ]]; then
-            echo "Unable to fetch expected addon version"
-            return 1
-        fi
-}
-
-# enableAddon() - disables the existing vpc-block-csi-driver addon and enables the 5.* addon from which snapshot is supported.
-enableAddon() {
-    ibmcloud ks cluster addon disable  vpc-block-csi-driver -f -c $1
-    # waiting for addon to be disabled
-    sleep 60s
-
-    # Enabling addon
-    ibmcloud ks cluster addon enable vpc-block-csi-driver  -c $1 --version $EXPECTED_VERSION
-    sleep 60s
-
-    # Check if controller server pod is up and running
-    kubectl get pods -n kube-system | grep ibm-vpc-block-csi-controller
-    if [[ $? != 0 ]]; then
-        error "Driver is not enabled"
-    fi
-
-    # List all pods in kube-system - if needed for debugging
-    kubectl get pods -n kube-system
-}
-
 # Print KUBECONFIG
 echo "KUBECONFIG is set to $KUBECONFIG"
 
@@ -101,22 +43,16 @@ error "Unable to fetch kubectl config"
 IFS='/' read -ra CLUSTER_NAME <<< "$IN"
 echo "Cluster Name - $CLUSTER_NAME"
 
-#Fetch cluster addon version
-addon_version=$(ibmcloud ks cluster addon ls -c $CLUSTER_NAME | grep vpc-block-csi-driver | awk '{print $2}')
-error "Unable to fetch vpc-block-csi-driver addon version"
-echo "Current vpc-block-csi-driver addon version - $addon_version"
+# Fetching pods in kube-system namespace to make sure, snapshot controller, and driver with sidecar is deployed.
+echo "Fetching pods in kube-system namespace"
+kubectl get pods -n kube-system
 
-#Fetching expected addon version
-fetchExpectedVersion
-error "Unable to fetch expected addon version"
+# Checking if controller pod is up
+echo "Checking if controller pod is up"
+kubectl get pods -n kube-system | grep ibm-vpc-block-csi-controller
+error "Driver is not enabled"
 
-#Check if the addon version is 5.+, if it is not disable the existing one, and enable 5.+. 
-if [[ "$addon_version" == "$EXPECTED_VERSION" ]]; then
-    echo "Expected addon version is enabled"
-else
-    enableAddon $CLUSTER_NAME
-fi
-
+# Sync ibm-csi-common
 mkdir -p "$GOPATH/src" "$GOPATH/bin" && sudo chmod -R 777 "$GOPATH"
 error "Unable to create src under GOPATH"
 mkdir -p $GOPATH/src/ibm-csi-common
