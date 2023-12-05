16411b78960c1a4b80e941c6648a98d3f41a885c
Prankul <84079249+prankulmahajan@users.noreply.github.com>
2023-12-05 12:06:07 +0530

    update interface lib

--- pkg/mountmanager/fake-safe-mounter.go
+++ pkg/mountmanager/fake-safe-mounter.go
@@ -36,16 +36,6 @@ func (*FakeNodeMounter) MountEITBasedFileShare(stagingTargetPath string, targetP
 	return nil
 }
 
-// UmountEITBasedFileShare ...
-func (*FakeNodeMounter) UmountEITBasedFileShare(targetPath string, requestID string) error {
-	return nil
-}
-
-// DebugLogsEITBasedFileShare...
-func (*FakeNodeMounter) DebugLogsEITBasedFileShare(requestID string) error {
-	return nil
-}
-
 // NewFakeNodeMounter ...
 func NewFakeNodeMounter() Mounter {
 	//Have to make changes here to pass the Mock functions
@@ -109,16 +99,7 @@ type FakeNodeMounterWithCustomActions struct {
 
 // MountEITBasedFileShare implements Mounter.
 func (*FakeNodeMounterWithCustomActions) MountEITBasedFileShare(stagingTargetPath string, targetPath string, fsType string, requestID string) error {
-	return nil
-}
 
-// UmountEITBasedFileShare ...
-func (*FakeNodeMounterWithCustomActions) UmountEITBasedFileShare(targetPath string, requestID string) error {
-	return nil
-}
-
-// DebugLogsEITBasedFileShare...
-func (*FakeNodeMounterWithCustomActions) DebugLogsEITBasedFileShare(requestID string) error {
 	return nil
 }
 
