1573da80e4bd0ddefa15be029f2cce520bc7db74
Guna K Kambalimath <Guna.Kambalimath@ibm.com>
2024-01-19 08:10:18 +0530

    Changing values to GB

--- pkg/utils/constants.go
+++ pkg/utils/constants.go
@@ -18,13 +18,18 @@
 package utils
 
 const (
-	// GiB in bytes
-	GiB = 1000 * 1000 * 1000
-
+	// KB ...
+	KB = 1e3
+	// MB ...
+	MB = 1e6
+	// GB ...
+	GB = 1e9
+	// TB ...
+	TB = 1e12
 	// MinimumVolumeSizeInBytes minimum size of the volume in bytes
-	MinimumVolumeSizeInBytes int64 = 10 * GiB
+	MinimumVolumeSizeInBytes int64 = 10 * GB
 	// MaximumVolumeSizeInBytes the max allowed capacity
-	MaximumVolumeSizeInBytes int64 = 2 * 1000 * GiB //2000GB = 2TB
+	MaximumVolumeSizeInBytes int64 = 2 * TB //2000GB = 2TB
 
 	// MinimumVolumeDiskSizeInGb minimum size of the volume in GB
 	MinimumVolumeDiskSizeInGb = 10
@@ -34,18 +39,6 @@ const (
 	DefaultVolumeDiskSizeinGb = 10
 	// MaxRetryAttemptForSessions ...
 	MaxRetryAttemptForSessions = 2
-)
-
-const (
-	_ = iota
-	// KB ...
-	KB = 1 << (10 * iota)
-	// MB ...
-	MB
-	// GB ...
-	GB
-	// TB ...
-	TB
 )
 
 const (
