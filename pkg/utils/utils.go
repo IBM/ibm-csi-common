7f896fb43669eecb43ec5a54a4b6c73e70164a12
Guna K Kambalimath <Guna.Kambalimath@ibm.com>
2024-02-05 12:58:16 +0530

    Converting value to GB (as expected by IAAS)

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -55,20 +55,27 @@ func NewNodeServiceCapability(cap csi.NodeServiceCapability_RPC_Type) *csi.NodeS
 	}
 }
 
+// TODO - remove the following code
+/*
 // RoundUpBytes rounds up the volume size in bytes upto multiplications of GiB
 // in the unit of Bytes
 func RoundUpBytes(volumeSizeBytes int64) int64 {
 	return roundUpSize(volumeSizeBytes, GiB) * GiB
 }
 
-// BytesToGiB converts Bytes to GiB
-func BytesToGiB(volumeSizeBytes int64) int {
-	return int(volumeSizeBytes / GiB)
-}
-
 // Check division by zero and int overflow
 func roundUpSize(volumeSizeBytes int64, allocationUnitBytes int64) int64 {
 	return (volumeSizeBytes + allocationUnitBytes - 1) / allocationUnitBytes
+}
+*/
+
+// BytesToGB converts Bytes to GB
+func BytesToGB(volumeSizeBytes int64) int {
+	return int(volumeSizeBytes / GB)
+}
+
+func GBToBytes(volumeSizeBytes int64) int64 {
+	return int64(volumeSizeBytes / GB)
 }
 
 // ListContainsSubstr Checks if subStr is there in mainStr
