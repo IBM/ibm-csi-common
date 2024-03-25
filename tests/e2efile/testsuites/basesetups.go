a9a50cd02beafb70671490f7d982ec151a376050
Sameer Shaikh <sameer.shaikh@ibm.com>
2024-03-25 09:22:27 +0530

    Change version

--- tests/e2efile/testsuites/basesetups.go
+++ tests/e2efile/testsuites/basesetups.go
@@ -103,7 +103,7 @@ func InitializeVPCClient() {
 	var apiKey string
 	var url string
 	var serviceURL string
-	version := "2024-02-06"
+	version := "2024-03-23"
 
 	if testEnv == "prod" {
 		apiKey = os.Getenv("IC_API_KEY_PROD")
