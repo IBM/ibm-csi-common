72cb10dab6cccb6c67c52f5b6a5b5462e3bb2832
Sameer Shaikh <sameer.shaikh@ibm.com>
2025-08-20 15:02:43 +0530

    Support for Trusted profile e2es
    Signed-off-by: Sameer Shaikh <sameer.shaikh@ibm.com>

--- tests/e2e/e2e.sh
+++ tests/e2e/e2e.sh
@@ -128,8 +128,6 @@ function check_trusted_profile_status {
         encoded=$(jq -r '.data["ibm-credentials.env"]' <<< "$secret_json")
         decoded=$(base64 --decode <<< "$encoded")
         profileID=$(echo $decoded | grep IBMCLOUD_PROFILEID | cut -d'=' -f3-)
-        echo "parsed $profileID"
-		echo "expected $expected_profile_id"
         if [[ "$profileID" == "$expected_profile_id" ]]; then
             echo -e "VPC-BLOCK-CSI-TEST: USING TRUSTED_PROFILE: TRUE" >> $E2E_TEST_SETUP
 			echo "***************************************************" >> $E2E_TEST_SETUP
